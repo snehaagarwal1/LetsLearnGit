d2da3fcc050a18ce0abfc0f0cf5d104b93c9fb24
Sneha Agarwal <snehaagarwalblp67@gmail.com>
2023-05-13 16:46:58 +0530

    added implementation for Div.java

--- Div.java
+++ Div.java
@@ -2,6 +2,6 @@ public class Div {
     public static void main(String args[]) {
         int a = 20;
         int b = 5;
-        System.out.println("Division = " + a / b);
+        System.out.println("Division= " + a / b);
     }
 }
